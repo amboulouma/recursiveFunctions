75e5d909e2d76722dbe2e7ea192df48f9e0d0e06
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-04-03 00:39:04 +0200

    added finabocci

---
+++ recursive_finabocci.py
@@ -0,0 +1,10 @@
+def finabocci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return finabocci(n-1) + finabocci(n-2)
+    
+while True:
+    print(finabocci(int(input())))
