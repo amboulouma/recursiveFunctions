56983c3c172a542249767e299bba96b2065ee7df
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-04-04 04:08:23 +0200

    added website

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ setup (
 
        # Declare your packages' dependencies here, for eg:
        install_requires=['foo>=3'],
-
        # Fill in these to make your Egg ready for upload to
        # PyPI
-       author='amine',
-       author_email='',
+       author='Amine M. Boulouma',
+       author_email='amine@boulouma.com',
 
        #summary = 'Just another Python package for the cheese shop',
-       url='',
+       url='http://amine.boulouma.com',
        license='',
        long_description='Long description of the package',
 
